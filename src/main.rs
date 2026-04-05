415b54969ebb2bb477dc8cf8ab4c2b7aaa9cdd26
G2-Games <ke0bhogsg@gmail.com>
2026-04-05 00:54:32 -0500

    Implemented web server API for rotator
    Co-authored-by: yetiodo <verybluebluejay@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+use std::{io::Error, num::ParseFloatError, ops::Neg, str::ParseBoolError};
+
 use rocket::{
-    get, routes
+    State, futures::lock::Mutex, get, routes, serde::json::Value
 };
+
+use crate::rotator::{Command, Direction, Rotator};
 
 pub mod rotator;
 
@@ -10,14 +14,32 @@ async fn main() {
         ..Default::default()
     };
 
+    let serial = serialport::new("/dev/ttyUSB0", 115_200)
+        .open()
+        .unwrap();
+    let rotator = Rotator::new(serial);
+    let rotator = Mutex::new(rotator);
+
     let rocket = rocket::build()
         .mount(
             "/",
             routes![
-                index
+                index,
+                set_position_vertical,
+                set_position_horizontal,
+                calibrate_vertical,
+                calibrate_horizontal,
+                move_direction,
+                move_vertical_steps,
+                move_horizontal_steps,
+                position,
+                calibrated,
+                halt,
+                version,
             ],
         )
         .configure(rocket_config)
+        .manage(rotator)
         .launch()
         .await;
 
@@ -27,4 +49,149 @@ async fn main() {
 #[get("/")]
 fn index() -> &'static str {
     "The server is running!"
+}
+
+type StatePort = State<Mutex<Rotator>>;
+
+/// Set a defined position for the rotator to move tow
+#[get("/dver?<degrees>")]
+async fn set_position_vertical(serial: &StatePort, degrees: f32) -> Result<(), std::io::Error> {
+    let mut rotator = serial.lock().await;
+
+    let cmd_string = rotator.send_command(Command::DegreesVertical, &[&format!("{:0.3}", degrees)])?;
+    rotator.validate_parse(&cmd_string)?;
+
+    Ok(())
+}
+
+/// Set a defined position for the rotator in the horizontal axis.
+#[get("/dhor?<degrees>")]
+async fn set_position_horizontal(serial: &StatePort, degrees: f32) -> Result<(), std::io::Error> {
+    let mut rotator = serial.lock().await;
+
+    let cmd_string = rotator.send_command(Command::DegreesHorizontal, &[&format!("{:0.3}", degrees.neg())])?;
+    rotator.validate_parse(&cmd_string)?;
+
+    Ok(())
+}
+
+/// Calibrates the vertical axis.
+#[get("/calv?<set>")]
+async fn calibrate_vertical(serial: &StatePort, set: bool) -> Result<(), std::io::Error> {
+    let mut rotator = serial.lock().await;
+
+    let cmd_string = if set {
+        rotator.send_command(Command::CalibrateVertical, &["SET"])?
+    } else {
+        rotator.send_command(Command::CalibrateVertical, &[])?
+    };
+
+    rotator.validate_parse(&cmd_string)?;
+    Ok(())
+}
+
+/// Calibrates the horizontal axis.
+#[get("/calh")]
+async fn calibrate_horizontal(serial: &StatePort) -> Result<(), Error> {
+    let mut rotator = serial.lock().await;
+
+    let cmd_string = rotator.send_command(Command::CalibrateHorizontal, &[])?;
+    rotator.validate_parse(&cmd_string)?;
+    Ok(())
+}
+
+/// Moves in a direction indefinitely specified by the command, or stops, if the command is to stop.
+#[get("/movc?<direction>")]
+async fn move_direction(serial: &StatePort, direction: Direction) -> Result<(), Error> {
+    let mut rotator = serial.lock().await;
+
+    let cmd_string = rotator.send_command(Command::CalibrateHorizontal, &[&direction.to_string()])?;
+    rotator.validate_parse(&cmd_string)?;
+    Ok(())
+}
+
+/// Moves by the specified number of steps in the vertical axis.
+#[get("/movv?<steps>")]
+async fn move_vertical_steps(serial: &StatePort, steps: i32) -> Result<(), Error> {
+    let mut rotator = serial.lock().await;
+
+    let cmd_string = rotator.send_command(Command::MoveVerticalSteps, &[&steps.to_string()])?;
+    rotator.validate_parse(&cmd_string)?;
+    Ok(())
+}
+
+/// Moves by the specified number of steps in the horizontal axis.
+#[get("/movh?<steps>")]
+async fn move_horizontal_steps(serial: &StatePort, steps: i32) -> Result<(), Error> {
+    let mut rotator = serial.lock().await;
+
+    let cmd_string = rotator.send_command(Command::MoveHorizontalSteps, &[&steps.to_string()])?;
+    rotator.validate_parse(&cmd_string)?;
+    Ok(())
+}
+
+#[get("/position")]
+/// Gets the current position for both the vertical and horizontal axes.
+async fn position(serial: &StatePort) -> Result<Value, Error> {
+    let mut rotator = serial.lock().await;
+
+    let cmd_string = rotator.send_command(Command::GetPosition, &[])?;
+    let value_list = rotator.validate_parse(&cmd_string)?
+            .ok_or(Error::other("ExpectedValue"))?;
+
+    if value_list.len() != 2 {
+        return Err(Error::other("InvalidResponse"))
+    }
+
+    let (v, h) = (
+        value_list[0].parse::<f32>().map_err(|e: ParseFloatError| Error::other(e.to_string()))?,
+        value_list[1].parse::<f32>().map_err(|e: ParseFloatError| Error::other(e.to_string()))?
+    );
+
+    Ok(rocket::serde::json::json!({
+        "vertical": v,
+        "horizontal": h,
+    }))
+}
+
+/// Gets the calibration status of the rotator. This must be true to use
+/// `set_position_vertical` and `set_position_horizontal`.
+#[get("/calibrated")]
+async fn calibrated(serial: &StatePort) -> Result<Value, Error> {
+    let mut rotator = serial.lock().await;
+
+    let cmd_string = rotator.send_command(Command::GetCalibrated, &[])?;
+
+    let value_list = rotator.validate_parse(&cmd_string)?
+        .ok_or(Error::other("ExpectedValue"))?;
+
+    let calibrated = value_list[0].parse::<bool>()
+        .map_err(|e: ParseBoolError| Error::other(e.to_string()))?;
+
+    Ok(rocket::serde::json::json!({
+        "calibrated": calibrated
+    }))
+}
+
+/// Immediately stops both motors by locking them to perform an emergency stop.
+#[get("/halt")]
+async fn halt(serial: &StatePort) -> Result<(), Error> {
+    let mut rotator = serial.lock().await;
+
+    let cmd_string = rotator.send_command(Command::Halt, &[])?;
+    rotator.validate_parse(&cmd_string)?;
+
+    Ok(())
+}
+
+/// Gets the current version of the software on the rotator.
+#[get("/version")]
+async fn version(serial: &StatePort) -> Result<String, Error> {
+    let mut rotator = serial.lock().await;
+
+    let cmd_string = rotator.send_command(Command::GetVersion, &[])?;
+
+    rotator.validate_parse(&cmd_string)?
+        .ok_or(Error::other("ExpectedValue"))
+        .map(|v| v[0].clone())
 }
--- src/rotator.rs
+++ src/rotator.rs
@@ -4,11 +4,11 @@
     clippy::cargo,
 )]
 
-use std::io::{BufWriter, Write as _};
-use core::{fmt::Display, num::ParseFloatError, str::ParseBoolError, ops::Neg as _};
+use std::io::{BufWriter, Error, Write as _};
+use core::fmt::Display;
+use rocket::FromFormField;
 
 use serialport::SerialPort;
-use thiserror::Error;
 
 /// Command that the rotator accepts.
 #[non_exhaustive]
@@ -54,7 +54,7 @@ impl Display for Command {
 
 /// Direction accepted by [`Command::Movement`].
 #[non_exhaustive]
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, FromFormField)]
 pub enum Direction {
     // Vertical
     Up,
@@ -65,6 +65,22 @@ pub enum Direction {
     Left,
     Right,
     StopHorizontal,
+}
+
+impl TryFrom<&str> for Direction {
+    type Error = ();
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        Ok(match value {
+            "UP" => Self::Up,
+            "DN" => Self::Down,
+            "SV" => Self::StopVertical,
+            "LT" => Self::Left,
+            "RT" => Self::Right,
+            "SH" => Self::StopHorizontal,
+            _ => return Err(())
+        })
+    }
 }
 
 impl Display for Direction {
@@ -81,27 +97,6 @@ impl Display for Direction {
 
         write!(f, "{cmd_text}")
     }
-}
-
-#[derive(Error, Debug)]
-pub enum Error {
-    #[error("the rotator returned an error: {0}")]
-    ResponseError(String),
-
-    #[error("the response from the rotator was invalid")]
-    InvalidResponse,
-
-    #[error("expected a value from the rotator but none recieved")]
-    ExpectedValue,
-
-    #[error("failed to parse value: {0}")]
-    ParseError(String),
-
-    #[error("the underlying serial port had an error")]
-    SerialError(#[from] serialport::Error),
-
-    #[error("there was an io error")]
-    IOError(#[from] std::io::Error),
 }
 
 /// A two-axis rotator, utilizing the
@@ -128,52 +123,6 @@ impl Rotator {
         })
     }
 
-    /// Position in degrees to move to in the vertical axis.
-    pub fn set_position_vertical(&mut self, pos: f32) -> Result<(), Error> {
-        let cmd_string = self.send_command(Command::DegreesVertical, &[&format!("{pos:0.3}")])?;
-        self.validate_parse(&cmd_string)?;
-        Ok(())
-    }
-
-    /// Position in degrees to move to in the horizontal axis.
-    pub fn set_position_horizontal(&mut self, pos: f32) -> Result<(), Error> {
-        let cmd_string = self.send_command(Command::DegreesHorizontal, &[&format!("{:0.3}", pos.neg())])?;
-        self.validate_parse(&cmd_string)?;
-        Ok(())
-    }
-
-    /// Calibrate vertical axis.
-    pub fn calibrate_vertical(&mut self, set: bool) -> Result<(), Error> {
-        let cmd_string = if set {
-            self.send_command(Command::CalibrateVertical, &["SET"])?
-        } else {
-            self.send_command(Command::CalibrateVertical, &[])?
-        };
-
-        self.validate_parse(&cmd_string)?;
-        Ok(())
-    }
-
-    /// Calibrate horizontal axis.
-    pub fn calibrate_horizontal(&mut self) -> Result<(), Error> {
-        let cmd_string = self.send_command(Command::CalibrateHorizontal, &[])?;
-        self.validate_parse(&cmd_string)?;
-        Ok(())
-    }
-
-    /// Moves in a direction indefinitely specified by the command, or stops, if the command is to stop.
-    pub fn move_direction(&mut self, direction: Direction) -> Result<(), Error> {
-        let cmd_string = self.send_command(Command::CalibrateHorizontal, &[&direction.to_string()])?;
-        self.validate_parse(&cmd_string)?;
-        Ok(())
-    }
-
-    /// Moves by the specified number of steps in the vertical axis.
-    pub fn move_vertical_steps(&mut self, steps: i32) -> Result<(), Error> {
-        let cmd_string = self.send_command(Command::MoveVerticalSteps, &[&steps.to_string()])?;
-        self.validate_parse(&cmd_string)?;
-        Ok(())
-    }
 
     /// Moves by the specified number of steps in the horizontal axis.
     pub fn move_horizontal_steps(&mut self, steps: i32) -> Result<(), Error> {
@@ -182,54 +131,8 @@ impl Rotator {
         Ok(())
     }
 
-    /// Gets the current position for both the vertical and horizontal axes.
-    pub fn position(&mut self) -> Result<(f32, f32), Error> {
-        let cmd_string = self.send_command(Command::GetPosition, &[])?;
-        let value_list = self.validate_parse(&cmd_string)?
-            .ok_or(Error::ExpectedValue)?;
-
-        if value_list.len() != 2 {
-            return Err(Error::InvalidResponse)
-        }
-
-        let (v, h) = (
-            value_list[0].parse().map_err(|e: ParseFloatError| Error::ParseError(e.to_string()))?,
-            value_list[1].parse().map_err(|e: ParseFloatError| Error::ParseError(e.to_string()))?
-        );
-
-        Ok((v, h))
-    }
-
-    /// Gets the calibration status of the rotator. This must be true to use
-    /// `set_position_vertical` and `set_position_horizontal`.
-    pub fn calibrated(&mut self) -> Result<bool, Error> {
-        let cmd_string = self.send_command(Command::GetCalibrated, &[])?;
-
-        let value_list = self.validate_parse(&cmd_string)?
-            .ok_or(Error::ExpectedValue)?;
-
-        value_list[0].parse()
-            .map_err(|e: ParseBoolError| Error::ParseError(e.to_string()))
-    }
-
-    /// Gets the current version of the software on the rotator.
-    pub fn version(&mut self) -> Result<String, Error> {
-        let cmd_string = self.send_command(Command::GetVersion, &[])?;
-        self.validate_parse(&cmd_string)?
-            .ok_or(Error::ExpectedValue)
-            .map(|v| v[0].clone())
-    }
-
-    /// Immediately stops both motors by locking them to perform an emergency stop.
-    pub fn halt(&mut self) -> Result<(), Error> {
-        let cmd_string = self.send_command(Command::Halt, &[])?;
-        self.validate_parse(&cmd_string)?;
-
-        Ok(())
-    }
-
     /// Send a command followed by arguments. Returns either an error if sending failed, or the
-    fn send_command(&mut self, command: Command, args: &[&str]) -> Result<String, std::io::Error> {
+    pub fn send_command(&mut self, command: Command, args: &[&str]) -> Result<String, std::io::Error> {
         let mut command_string = BufWriter::new(Vec::new());
 
         self.port.write_all(command.to_string().as_bytes())?;
@@ -260,14 +163,14 @@ impl Rotator {
     }
 
     /// Read the rotator response and determine errors or validation
-    fn validate_parse(&mut self, command_string: &str) -> Result<Option<Vec<String>>, Error> {
+    pub fn validate_parse(&mut self, command_string: &str) -> Result<Option<Vec<String>>, Error> {
         let mut response_string = String::new();
 
         // Fill up the result string with what the rotator spits out
         let mut buffer = [0; 2048];
         while let Ok(num_read) = self.port.read(&mut buffer) && num_read != 0 {
             let Ok(read_buffer) = str::from_utf8(&buffer[..num_read]) else {
-                return Err(Error::InvalidResponse)
+                return Err(Error::other("invalid response"))
             };
 
             response_string.push_str(read_buffer);
@@ -278,15 +181,15 @@ impl Rotator {
 
         // The first line should be an echo of what was sent
         if response_lines[0] != command_string.trim() {
-            return Err(Error::InvalidResponse)
+            return Err(Error::other("invalid response"))
         }
 
         // Split the second line into a status followed by the return values
         let response_list: Vec<&str> = response_lines[1].splitn(2, ' ').collect();
         match response_list[0] {
-            "ERR" => return Err(Error::ResponseError(response_list[1].to_string())),
+            "ERR" => return Err(Error::other(response_list[1].to_string())),
             "OK" => (),
-            _ => return Err(Error::InvalidResponse)
+            _ => return Err(Error::other("invalid response"))
         }
 
         // Split the return values further
